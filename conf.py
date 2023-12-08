f677bfb7193ae2de8c07ed92cdab8b7eaa7fe62a
Obarrie <113312631+Obarrie@users.noreply.github.com>
2023-12-07 21:12:22 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("./_ext"))
+
+extensions = ['todo']
+
+todo_include_todos = False
+
+
 # # -- Project information -----------------------------------------------------
 
 # project = 'Site Title'
@@ -15,14 +25,7 @@
 # # Add any Sphinx extension module names here, as strings. They can be
 # # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # # ones.
-import os
-import sys
 
-sys.path.append(os.path.abspath("./_ext"))
-
-extensions = ['todo']
-
-todo_include_todos = False
 
 # # "sphinxext.rediraffe",
 
